3c062d1ff810e9f7a78a8f2b32b68940c6d4eb8f
Joel Kong <65745644+JoelKong@users.noreply.github.com>
2022-10-15 23:17:46 +0800

    Modified the game to stop after a number of turns

--- ScoreKeeper/apptry.js
+++ ScoreKeeper/apptry.js
@@ -1,10 +1,12 @@
 const p1 = {
+    index: 0,
     score: 0,
     scoreDisplay: document.querySelector('#p1Display'),
     button: document.querySelector('#p1Button')
 }
 
 const p2 = {
+    index: 1,
     score: 0,
     scoreDisplay: document.querySelector('#p2Display'),
     button: document.querySelector('#p2Button')
@@ -14,19 +16,35 @@ const playingTo = document.querySelector('#playto');
 const reset = document.querySelector('#reset');
 let winningScore = 3;
 let winLose = false;
+let totalScore = [0, 0];
 
 
 function updateScores(player, opponent) {
     if (!winLose) {
         player.score += 1;
         player.scoreDisplay.textContent = player.score;
+        totalScore[player.index] = player.score;
+        let totalPlayed = totalScore.reduce((total, current) => total + current)
 
-        if (player.score === winningScore) {
+        if (totalPlayed === winningScore) {
+            //if (player.score === winningScore) {
             winLose = true;
-            player.scoreDisplay.classList.add('has-text-success');
-            opponent.scoreDisplay.classList.add('has-text-danger');
             player.button.disabled = true;
             opponent.button.disabled = true;
+            if (player.score > opponent.score) {
+                player.scoreDisplay.classList.add('has-text-success');
+                opponent.scoreDisplay.classList.add('has-text-danger');
+            }
+            else if (player.score < opponent.score) {
+                player.scoreDisplay.classList.add('has-text-danger');
+                opponent.scoreDisplay.classList.add('has-text-success');
+            }
+            else {
+                player.scoreDisplay.classList.add('has-text-danger');
+                opponent.scoreDisplay.classList.add('has-text-danger');
+                setTimeout(() => alert('Its a draw!'), 200);
+            }
+
         }
     }
 }
@@ -37,7 +55,7 @@ p1.button.addEventListener('click', () => updateScores(p1, p2));
 
 p2.button.addEventListener('click', () => updateScores(p2, p1));
 
-playingTo.addEventListener('change', function () {
+playingTo.addEventListener('change', function () { //cant use arrows for this.value
     winningScore = parseInt(this.value); //this.value will be a string at first
     resetfunc();
 })
@@ -48,6 +66,7 @@ reset.addEventListener('click', resetfunc);
 
 function resetfunc() {
     winLose = false;
+    totalScore = [0, 0];
     for (let p of [p1, p2]) {
         p.score = 0;
         p.scoreDisplay.innerText = 0;
@@ -55,5 +74,7 @@ function resetfunc() {
         p.button.disabled = false;
     }
 }
+
+
 
 
